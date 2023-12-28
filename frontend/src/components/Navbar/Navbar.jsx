b4bad15b9670675745e28a2bad08f6342efe8a1e
Rushalee <rushaleedas@gmail.com>
2023-12-29 00:28:55 +0530

    assets

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="nav-logo">
+            <img src={logo} alt="" />
+            <p>shop</p>
+        </div>
+        <ul className="nav-menu">
+            <li></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
