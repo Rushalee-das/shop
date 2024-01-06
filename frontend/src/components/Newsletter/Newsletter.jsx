915403964a7ca9de37023395e0c92d4c6faa5dec
Rushalee <rushaleedas@gmail.com>
2024-01-07 01:26:43 +0530

    nesletter added

--- frontend/src/components/Newsletter/Newsletter.jsx
+++ frontend/src/components/Newsletter/Newsletter.jsx
@@ -4,7 +4,12 @@ import './Newsletter.css'
 const Newsletter = () => {
   return (
     <div className='newsletter'>
-        
+        <h1>Get Exclusive Offers On Your Email</h1>
+        <p>Subscribe to our newsletter and stay updated</p>
+        <div>
+            <input type="email" placeholder='Your Email Id' />
+            <button>Subscribe</button>
+        </div>
     </div>
   )
 }
--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero/Hero'
 import Popular from '../components/Popular/Popular'
 import Offers from '../components/Offers/Offers'
 import NewCollections from '../components/NewCollections/NewCollections'
+import Newsletter from '../components/Newsletter/Newsletter'
 
 const Shop = () => {
   return (
@@ -11,6 +12,7 @@ const Shop = () => {
       <Popular/>
       <Offers/>
       <NewCollections/>
+      <Newsletter/>
     </div>
   )
 }
